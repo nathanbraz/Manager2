55fe756377d903eb3b4f841376d59e147f9d06e2
Nathan <nathanbrz09@gmail.com>
2024-06-08 14:31:57 -0300

    Add class Stock and Comment

---
+++ API/Models/Stock.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; } = String.Empty;
+        public string CompanyName { get; set; } = String.Empty;
+        [Column(TypeName = "decimal(18.2)")]
+        public decimal Purchase { get; set; }
+        public decimal LastDiv { get; set; }
+        public string Industry { get; set; } = string.Empty;
+        public long MarketCap { get; set; }
+        public List<Comment> Comment { get; set; } = new List<Comment>();
+    }
+}
